86643782def9bf753724d4d33a1cb5ebb7878734
Brian Glen <br.glen@yahoo.com>
2025-05-30 23:26:33 -0400

    App adjustments to make grids work

--- wire-harness-app/src/index.js
+++ wire-harness-app/src/index.js
@@ -1,6 +1,11 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('node:path');
 
+// Disable GPU hardware acceleration
+app.disableHardwareAcceleration();
+
+require('electron-reload')(path.join(__dirname));
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
---
+++ wire-harness-app/src/renderer.js
@@ -0,0 +1,15 @@
+import { initializeGrid } from './wire-harness-core/grid.js';
+
+window.onload = () => {
+    const canvasElement = document.getElementById("fabricCanvas");
+    // Create a Fabric canvas linked to a <canvas> element with id "fabricCanvas"
+    const canvas = new fabric.Canvas('fabricCanvas', {
+        backgroundColor: '#ffffff',
+        selection: false,
+    });
+
+    // Grid Presets
+    const gridPresets = [10, 25, 50, 100];
+
+    initializeGrid(canvas, canvasElement, gridPresets);
+};
